8c51e96dba53e161c9f5abb2dd4199fa90fbcfd9
Cihangir SAVAS <cihangir@koding.com>
2016-07-13 01:45:40 +0300

    data: add documentation

--- data/install_java.sh
+++ data/install_java.sh
@@ -28,5 +28,11 @@ alternatives --install /usr/bin/java jar /usr/java/latest/bin/java 200000
 alternatives --install /usr/bin/javaws javaws /usr/java/latest/bin/javaws 200000
 alternatives --install /usr/bin/javac javac /usr/java/latest/bin/javac 200000
 
+# It's NOT a good idea to change /etc/profile unless you know what you
+# are doing. It's much better to create a custom.sh shell script in
+# /etc/profile.d/ to make custom changes to the environment, as this
+# will prevent the need for merging in future updates.
 echo "export JAVA_HOME=/usr/java/latest" > /etc/profile.d/java.sh
+# on the other hand /etc/profile.d/eb_envvars.sh uses /usr/java/latest as
+# JAVA_HOME if directory is present
 source /etc/profile
