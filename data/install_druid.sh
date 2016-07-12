b2fd0db0351983dbc967e2c68df4b20dda70e286
Cihangir SAVAS <cihangir@koding.com>
2016-07-13 02:01:46 +0300

    data: create druid user and group then give required permissions

--- data/install_druid.sh
+++ data/install_druid.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -o errexit
 
 DRUID_VERSION=0.9.1.1
@@ -7,3 +6,10 @@ DRUID_VERSION=0.9.1.1
 wget -q http://static.druid.io/artifacts/releases/druid-$DRUID_VERSION-bin.tar.gz -O /tmp/druid.tar.gz
 tar xfz /tmp/druid.tar.gz -C /opt
 mv /opt/druid-* /opt/druid
+
+groupadd -r druid
+useradd -g druid -M -r druid
+
+mkdir -p /var/{lib,log}/druid
+chown -R druid:druid /var/{lib,log}/druid
+chown -R druid:druid /opt/druid
